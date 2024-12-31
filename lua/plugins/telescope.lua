c4bc36944ff9eef1a694294e8dbb826ced6aa1ea
= <example@example.com>
2024-12-31 12:46:27 -0400

    telescope ignorar vendor e node_modules

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,11 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
 
         config = function()
+            require('telescope').setup({
+                defaults = {
+                    file_ignore_patterns = { '^node_modules/', '^vendor/' },
+                },
+            })
             local builtin = require('telescope.builtin')
 
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
